cda2430f277d4e7ce64f14e2f85f384986e1a30a
samyakgh177 <samyakg17@gmail.com>
2025-03-23 05:06:03 +0530

    conflict resoultion

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -58,11 +58,15 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
+        PreviousMonthButton: (props) => (
+          <button {...props}>
+            <ChevronLeft className="size-4" />
+          </button>
         ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
+        NextMonthButton: (props) => (
+          <button {...props}>
+            <ChevronRight className="size-4" />
+          </button>
         ),
       }}
       {...props}
